f5eaf184232e30b17aa81388e430dc4cdaef6c44
Jayant Patil <piyushjayantpatil@gmail.com>
2023-12-10 22:16:30 -0500

    minor edits to fpgrowth

--- fpgrowth.py
+++ fpgrowth.py
@@ -12,14 +12,17 @@ import math
 
 ASSOCIATION_RULE_THRESHOLD = 0.8
 SUPPORT_THRESHOLD_PERCENTAGE = 10
-NUM_OF_FREQUENT_PATTERNS = 30
+MAX_NUM_OF_FREQUENT_PATTERNS = 30
+
+MIN_LENGTH_OF_PATTERN = 3
+# columns_to_mine = ["Collision Type", "Weather", "Surface Condition", "Light", "Traffic Control"]
+columns_to_mine = ["Injury Severity", "Agency Name", "ACRS Report Type", "Route Type", "Cross-Street Type"]
 
 ASSOCIATION_NUM_ATTRIBUTES = 4
 SUPPORT_NUM_ATTRIBUTES = 5
 
 
 df = pd.read_csv('data/crash_reporting_drivers_data_sanitized.csv')
-columns_to_mine = ["Collision Type", "Weather", "Surface Condition", "Light", "Traffic Control"]
 df = df[columns_to_mine]
 df = df.dropna()
 df = df.values.tolist()
@@ -31,6 +34,10 @@ if ASSOCIATION_NUM_ATTRIBUTES > len(columns_to_mine):
 if SUPPORT_NUM_ATTRIBUTES > len(columns_to_mine):
     print("support_num_attributes is greater than the length of columns_to_mine")
     exit(1)
+if MIN_LENGTH_OF_PATTERN > len(columns_to_mine):
+    print("The minimum number of attributes in frequent pattern is \
+          greater than the length of columns_to_mine")
+    exit(1)
 
 # Calculate support_threshold from SUPPORT_THRESHOLD_PERCENTAGE
 support_threshold = math.floor(len(df) * (SUPPORT_THRESHOLD_PERCENTAGE / 100))
@@ -39,10 +46,22 @@ patterns = pyfpgrowth.find_frequent_patterns(df, support_threshold)
 rules = pyfpgrowth.generate_association_rules(patterns, ASSOCIATION_RULE_THRESHOLD)
 
 # Print top 30 frequent patterns
-top_patterns = sorted(patterns.items(), key=lambda x: x[1], reverse=True)[:NUM_OF_FREQUENT_PATTERNS]
-print(f"Top {NUM_OF_FREQUENT_PATTERNS} Frequent Patterns:")
+# top_patterns = sorted(patterns.items(), key=lambda x: x[1], reverse=True)[:NUM_OF_FREQUENT_PATTERNS]
+# print(f"Top {NUM_OF_FREQUENT_PATTERNS} Frequent Patterns:")
+
+top_patterns = sorted(patterns.items(), key=lambda x: x[1], reverse=True)
+print(f"\nTop Frequent Patterns:\n")
+counter = 0
 for pattern, support in top_patterns:
-    print(f"{pattern}: support = {support}")
+    if counter == MAX_NUM_OF_FREQUENT_PATTERNS:
+        break
+    if len(pattern) == MIN_LENGTH_OF_PATTERN:
+        print(f"{pattern}: support = {support}")
+        counter += 1
+if counter < MAX_NUM_OF_FREQUENT_PATTERNS:
+    print(f"\nOnly {counter} frequent patterns found with \n\tMIN_LENGTH_OF_PATTERN = {MIN_LENGTH_OF_PATTERN}\n\tMAX_NUM_OF_FREQUENT_PATTERNS = {MAX_NUM_OF_FREQUENT_PATTERNS}")
+else:
+    print(f"\nTop {MAX_NUM_OF_FREQUENT_PATTERNS} Frequent Patterns printed")
 
 print("\n______________________________________________________\n")
 
